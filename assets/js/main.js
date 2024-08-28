193bf4a1944a9191afb196efe9178fbd386e9348
markeloss <eriquepinon@gmail.com>
2024-08-27 21:44:21 -0300

    adicionando imagens dinamicas

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,8 +26,14 @@ function updateSoftSkills(profileData) {
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li> ${skill}</li>`).join('')
 }
 
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<img src="${skill.logo}" alt="${skill.name}" title="${skill.name}">`).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
+    updateHardSkills(profileData)
 })()
